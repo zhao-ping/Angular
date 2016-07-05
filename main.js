35a3c4936a98163fa3345ce57f6a05ae003c588c
zhao ping <2362331574@qq.com>
2016-07-05 16:13:39 +0800

    angular

--- main.js
+++ main.js
@@ -2,10 +2,6 @@
  * Created by zp on 16-7-5.
  */
 var routerApp=angular.module('angularRouter',['ngRoute']);
-routerApp.config('$routerProvider',function ($routerProvider) {
-    $routerProvider
-        .when('/',{template:"11111111111111"})
-        .when('/list',{template:"22222222222222"})
-        .when('/container',{template:"33333333333333333333"})
-        .otherwise({redirectTo:'/'})
+routerApp.constructor('routerCtrol',function($scope){
+    $scope.show=true;
 })
