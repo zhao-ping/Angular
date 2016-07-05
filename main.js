70621a1b0f4cc29b10901ba1077d4f2d05964d8a
zhao ping <2362331574@qq.com>
2016-07-05 16:36:06 +0800

    angular form editor

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 /**
  * Created by zp on 16-7-5.
  */
-var routerApp=angular.module('angularRouter',['ngRoute']);
-routerApp.constructor('routerCtrol',function($scope){
-    $scope.show=true;
-})
+var routerApp=angular.module('angularRouter',[]);
+routerApp.controller('routerCtrl',function($scope){
+    $scope.items=[
+        {type:'text'},
+        {type:'date'}
+    ];
+    $scope.isShow=true;
+    $scope.alertShow=false;
+    $scope.toggle=function () {
+        $scope.isShow=!$scope.isShow;
+    };
+    $scope.toggle1=function () {
+        $scope.alertShow=!$scope.alertShow;
+    };
+    $scope.deleteitem=function(index){
+        $scope.items.splice(index,1);
+    };
+    $scope.addItem=function () {
+        var itemType=jQuery('input[type=radio]:checked').val();
+        console.log(itemType);
+        var newElement={type:itemType};
+        $scope.items.push(newElement);
+    }
+});
